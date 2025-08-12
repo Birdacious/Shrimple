7425cf7d0a9dc9fa408b433061edf5284393f0b2
Birdacious <bird@birdacious.org>
2025-08-12 17:41:37 -0400

    Shrimplify

--- Shrimple.py
+++ Shrimple.py
@@ -1,13 +1,6 @@
-"""
-@Jalexu on discord had an idea for fingerspelling using the whole keyboard, not just one letter at a time
-Key considerations were that it wasn't tailored for English, as this is mostly for foreign words/names
-"""
-
-entry_strokes={
+entry_strokes={ # TODO maybe
     "HR"        : {"prefix":"",
                    "suffix":""},
-    #"SHREUFRPL" : {"prefix":"",
-    #               "suffix":""},
 
     "TPH*"      : {"prefix":"{-|}", #capitalise first letter
                    "suffix":""},
@@ -15,20 +8,61 @@ entry_strokes={
     "KAPS"      : {"prefix":"{mode:caps}", #capitalise everything
                    "suffix":"{mode:reset}"}
 }
-
-#dedicated key settings:
-dedicated_key = '+'             #Instead of a starter stroke
-make_words_done_with_dedicated_key_exit_immediately = True
-joiner_strokes={ #If Shrimple automatically exits, maybe you wanna use KWR to stay in Shrimple
-    "left-hand joiner" : "^SKWR",
-    #"right-hand joiner" : "FPL"
+exit_strokes={
+    "HR"
 }
+overrides={
+  # RHS lone spellings are symbols. If you want to type a lone letter, use LHS.
+  "#":   "0",
+  "-BG": "*",
+  "-P":  ".",
+  "-PL": "'",
+  "-T":  "<",
+  "-G":  "-",
+  "-S":  "_",
+  "-D":  ",",
 
+  "-F":  " ",
 
-make_starter_letters_have_left_to_right_priority = True
-starter_letter={
+  # RHS lone spellings + -L. TODO: #L is a free space, just repeating for \" for now.
+  "#-L":    "\"",
+  "*LG":  "^",
+  "-PLZ":   "!",
+  "-PLSZ": "(",
+  "-LT":   ")",
+  "-LG":   "|",
+  "-LS":   "$",
+  "-LD":   "/",
+
+  "-FR":   "{#Tab}",
+
+  # RHS lone double-taps
+  "HA*ERB":   "#",
+  "OEU":      "/",
+  "STPH-FPT": ":",
+  "KR*GS":    "\"",
+  "TKPWR*PB": ">",
+  "PHR*US":   "+",
+  "KW*":      "=",
+  "STPH*FPT": ";",
+
+  # RHS lone double-taps + -L
+  "HA*ERBL":   "@",
+  "OEUL":      "%",
+  "STPH-FPLT": "`",
+  "KR*LGS":    "[",
+  "TKPWR*PBL": "]",
+  "PHR*ULS":   "&",
+  "KW*L":      "~",
+  "STPH*FPLT": "?",
+
+  # RHS lone cross-deadzone taps + -L
+  "-PLT": "\\{",
+  "-PBLT": "\\}"
+
+}
+starters={
     "" : "",
-    "^":"{^^}",
 
     # A        a
     "PW":     "b",
@@ -41,7 +75,7 @@ starter_letter={
     # EU       i
     "SKWR":   "j",
     "K":      "k",
-    "HR":     "l", # If alone, exits Shrimple.
+    #"HR":     "l", # If alone, exits Shrimple.
     "PH":     "m",
     "TPH":    "n",
     # O        o
@@ -88,6 +122,7 @@ starter_letter={
     "SKW":    "squ",
     #"ST":    "st",  # Overridden by "6"
     "STR":    "str",
+    "TH":     "th",
     "THR":    "thr",
     "TR":     "tr",
 
@@ -104,7 +139,6 @@ starter_letter={
     "SH":    "8",
     "STK":   "9",
 }
-
 vowels={
     "-":"",
     "":"",
@@ -137,9 +171,7 @@ vowels={
     "OE":   "o[e]",
     "OU":   "ou",
 }
-
-make_ender_letters_have_left_to_right_priority = True
-ender_letter={
+enders={
     "":"",
     #"*":"", #asterisk on its own is invalid
 
@@ -205,7 +237,7 @@ ender_letter={
     "#F":  "1",
     "FBG": "2",
     "FP":  "3",
-    "FPH": "4",
+    "FPL": "4",
     "FL":  "5",
     "FT":  "6",
     "FG":  "7",
@@ -213,382 +245,24 @@ ender_letter={
     "FD":  "9",
 }
 
-
-
-strokes_you_can_use_to_exit_shrimple_with=[
-
-    #"SHREUFRPL", # "Shrimple" again
-    "HR",        #
-    "PHO-UPL",   # Mouse mode
-    "PHRO-LG",   # Toggle Plover
-
-    #punctuation
-    "TK-LS",    #no space
-    "S-P",      #space
-    "KPA",      #caps
-    "KPA*",     #caps no space
-    "R-R",      #enter
-    "TP-PL",    #.
-    "TA*B",     #can't remember
-    "TPWA*",    #left hand tab
-    #"R*",       #left hand return
-    "KW-PL",    #?
-    "TP-BG",    #o
-    "KW-BG",    #,
-    "AEZ",      #'s
-    "A*ES",     #s'
-    "AES",      #'s
-    "HAERB",    ##
-    "KWRA*T",   #@
-    "P-P",      #.
-    "H-N",      #-
-    "H*N",      #-
-    "TPHO*FRL", #normal
-    "*",        #delete
-    "PW*FP",    #control backspace
-    "EFBG",     #escape
-    "^*",
-    "^S",
-    "TKUPT",
-    "STKHR",    #delete
-    "TKHR",     #backspace
-
-    #navigation
-    "STPH-R",
-    "STPH-RB",
-    "STPH-P",
-    "STPH-B",
-    "STPH-BG",
-    "STPH-G",
-    "STPH-FR",
-    "STPH-LG",
-    "PW-FP",
-
-]
-
-left_finger_chords_you_can_use_to_exit_shrimple_with={
-    "STKPWHR", # Modifier mode
-}
-
-left_hand_chords_you_can_use_to_exit_shrimple_with={}
-right_finger_chords_you_can_use_to_exit_shrimple_with={}
-left_finger_chords_you_can_use_during_the_final_stroke_to_exit_shrimple_with=[]
-right_finger_chords_you_can_use_during_the_final_stroke_to_exit_shrimple_with=[]
-
-dedicated_key_you_can_use_during_the_final_stroke_to_exit_shrimple_with = "$"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import re
-
-LONGEST_KEY = 40
-
-
-if make_starter_letters_have_left_to_right_priority:
-    print('@Harrri to remind him he forgot this part')
-
-
-if make_ender_letters_have_left_to_right_priority:
-    print('@Harrri to remind him he forgot this part')
-
-
-numbers_to_letters = {
-    "1": "S",
-    "2": "T",
-    "3": "P",
-    "4": "H",
-    "5": "A",
-    "6": "F",
-    "7": "P",
-    "8": "L",
-    "9": "T",
-    "0": "O"
-    }
-
-#when the hash key is pressed, it interferes with the letter, I'm not interested in that, so I'm just undoing that here
-def aericks_denumberizer(old_outline):
-
-    old_strokes = old_outline.split("/")
-    new_strokes = []
-
-    for stroke in old_strokes:
-
-        new_strokes.append(stroke)
-
-        for match in numbers_to_letters.keys():
-
-            if match in stroke:
-
-                if new_strokes[-1][0] != "#":
-                    new_strokes[-1] = "#" + new_strokes[-1]
-
-                new_strokes[-1] = new_strokes[-1].replace(match, numbers_to_letters[match])
-
-        if new_strokes == []:
-            new_strokes = old_strokes
-
-    return "/".join(new_strokes)
-
-
-
-def slices(s, n):
-    if n == 1:
-        yield [s]
-    else:
-        for split in range(1, len(s)):
-            for others in slices(s[split:], n-1):
-                yield [s[:split]] + others
-
-def star_positions(parts):
-    for i in range(len(parts)):
-        yield ["*" + s if i == j else s for j, s in enumerate(parts)]
-
-#this function constructs the stroke recursively, if the stroke isn't found, then a match using 2 strokes is searched for, then 3, then 4 etc, etc
-def construct_stroke_2(target_chord, chord_dictionary, has_asterisk = False):
-    for num_parts in range(1, min(6, len(target_chord)+1)):
-        # When the target chord is made of `num_parts` definitions that are in the dictionary
-        for parts in slices(target_chord, num_parts):
-            if not has_asterisk:
-                if all(nth_half in chord_dictionary for nth_half in parts):
-                    return "".join(chord_dictionary[nth_half] for nth_half in parts)
-            else:
-                for starred_parts in star_positions(parts):
-                    if all(nth_half in chord_dictionary for nth_half in starred_parts):
-                        return "".join(chord_dictionary[nth_half] for nth_half in starred_parts)
-
-    return ""
-
-def lookup(strokes, construct_stroke=construct_stroke_2):
-
-    output_string= ""
-
-
-    for stroke_number, stroke in enumerate(strokes):
-
-        if stroke_number == 0:
-            if not dedicated_key in stroke:
-                stroke_valid = False
-                if stroke in entry_strokes:
-                    stroke_valid = True
-                if not stroke_valid:
-                    raise KeyError
-                if len(strokes)==1:
-                    return '{Shrimple}'
-        else:
-            if not dedicated_key in stroke:
-                stroke_valid = False
-                if stroke in entry_strokes:
-                    stroke_valid = True
-                if stroke_valid:
-                    raise KeyError
-                
-
-            
-
-        if stroke==dedicated_key:
-            raise KeyError
-
-
-        if stroke in strokes_you_can_use_to_exit_shrimple_with:
-            raise KeyError
-
-
-        match = re.fullmatch(r'(#?\^?S?T?K?P?W?H?R?)(A?O?)(\*?\-?E?U?)(F?R?P?B?L?G?T?S?D?Z?)', stroke.replace(dedicated_key,""))
-        if match:
-
-            if match[1] in left_finger_chords_you_can_use_to_exit_shrimple_with:
-                raise KeyError
-            if match[1]+match[2] in left_hand_chords_you_can_use_to_exit_shrimple_with:
-                raise KeyError
-            if match[4] in right_finger_chords_you_can_use_to_exit_shrimple_with:
-                raise KeyError
-
-
-            if ((match[1] in left_finger_chords_you_can_use_during_the_final_stroke_to_exit_shrimple_with) or (match[4] in right_finger_chords_you_can_use_during_the_final_stroke_to_exit_shrimple_with) or (dedicated_key_you_can_use_during_the_final_stroke_to_exit_shrimple_with in match[1])) and not stroke_number+1 == len(strokes):
-                futurematch = re.fullmatch(r'(#?\^?S?T?K?P?W?H?R?)(A?O?)(\*?\-?E?U?)(F?R?P?B?L?G?T?S?D?Z?)', strokes[stroke_number+1].replace(dedicated_key,""))
-                if not (futurematch[1] in left_finger_chords_you_can_use_during_the_final_stroke_to_exit_shrimple_with or futurematch[4] in right_finger_chords_you_can_use_during_the_final_stroke_to_exit_shrimple_with):
-                    raise KeyError
-
-
-        if make_words_done_with_dedicated_key_exit_immediately and dedicated_key in strokes[0]:# and not "^" in strokes[0]:
-            if stroke_number == 0 and not dedicated_key in stroke:
-                raise KeyError
-            elif not stroke_number == 0 and not (match[1] == joiner_strokes['left-hand joiner'] or match[4] == joiner_strokes["right-hand joiner"]):
-                raise KeyError
-            
-
-
-
-
-
-    if (len(strokes)) == 1 and dedicated_key not in strokes[0]:
-        if strokes[0]==entry_strokes['starterattached']:
-            return ("{^}")
-        else:
-            return("{^ ^}")
-
-    for stroke_number in range(len(strokes)):
-
-
-
-        if not stroke_number == 0 and (strokes[stroke_number] in entry_strokes.values() or dedicated_key in strokes[stroke_number]):
-            raise KeyError
-
-        #match the strokes
-        match= re.fullmatch(
-            #dissect the string to starter_letters, vowels and ender_letters
-            r'\+?(#?)(\^?S?T?K?P?W?H?R?)(A?O?\*?\-?E?U?)(F?R?P?B?L?G?T?S?D?Z?)',
-
-            #this string:
-            aericks_denumberizer(strokes[stroke_number].replace(dedicated_key,"")))
-
-        if not match:
-            raise KeyError
-
-
-        start_thing=construct_stroke(match[2], starter_letter)
-        #if there's an asterisk, you gotta find where to implement it
-        if "*" in match[3]:
-
-            end_thing=construct_stroke(match[4], ender_letter, has_asterisk = True)
-
-
-            if end_thing == "":
-
-                end_thing = construct_stroke(match[4], ender_letter, has_asterisk = False)
-                middle_thing=construct_stroke(match[3].replace("*",""), vowels, has_asterisk=True)
-
-                if middle_thing==construct_stroke(match[3].replace("*",""), vowels, has_asterisk=False):
-                    start_thing=construct_stroke(match[2], starter_letter, has_asterisk=True)
-
-            else:
-                middle_thing=construct_stroke(match[3].replace("*",""), vowels)
-
-
-
-
-        else:
-            end_thing=construct_stroke(match[4], ender_letter)
-            middle_thing=construct_stroke(match[3], vowels)
-
-        #now do stuff with like [e]:
-        if '[e]' in middle_thing:
-            end_thing+="[e]"
-            middle_thing=middle_thing.replace("[e]","")
-
-        if '[e]' in end_thing:
-            end_thing+="e"
-            end_thing=end_thing.replace("[e]","")
-
-        if '[y]' in end_thing:
-            end_thing+="{^y}"
-            end_thing=end_thing.replace("[y]","")
-
-        if '[s]' in end_thing:
-            end_thing+="{^s}"
-            end_thing=end_thing.replace("[s]","")
-
-
-        if not stroke_number==0 or (dedicated_key in strokes[0]):
-            if "#" in match[1]:
-                output_string+=(
-                    (start_thing+
-                    middle_thing+
-                    end_thing
-                    ).capitalize())
-
-            else:
-                output_string+=(
-                    start_thing+
-                    middle_thing+
-                    end_thing
-                    )
-
-
-    if strokes[0] in entry_strokes:
-        return entry_strokes[strokes[0]]["prefix"] + output_string + entry_strokes[strokes[0]]["suffix"]
-    return output_string
-
-def test(x,construct_stroke=construct_stroke_2):
-    try:
-        return lookup(x, construct_stroke=construct_stroke)
-    except Exception as e:
-        return e
-
-
-print(("DIFFERENCES: ", {
-    x: results for x in [
-        ("+KAPZ","KWROU"),
-        ("+WA*U"),
-        ("KAPS", "STA*RTD"),
-        ("KWR", "A*"),
-        ("KAPS", "STKHRA*RTD"),
-        ("KAPS", "WA*TD"),
-        ("KAPS", "WA*TD", "KWRAL"),
-        ("KAPS", "WA*TD", "KWRAL", "PAL")] 
-        if (results := (test(x), test(x,  construct_stroke=construct_stroke_2)))
-        if str(results[0]) != str(results[1])
-        }))
+LONGEST_KEY = 1
+def lookup(chord):
+    if len(chord) != 1: raise KeyError
+    assert len(chord) <= LONGEST_KEY
+
+    if chord[0] ==      'HR': return "{PLOVER:END_SOLO_DICT}"
+    if chord[0] ==  'PHOUPL': return "{PLOVER:END_SOLO_DICT}{PLOVER:SOLO_DICT:+mouse_mode.json,+suppress_untranslates.py,+mouse-modifiers.py,+repeat_last.json}"
+    if chord[0] in overrides:
+      out = overrides[chord[0]]
+      if '{' == out[0]: return        out 
+      else            : return '{^' + out + '^}'
+
+    match = re.fullmatch(r'(#?)([STKPWHR]*)([AO]*)[-]?([*]?)([EU]*)([FRPBLGTSDZ]*)', chord[0])
+    if match is None: raise KeyError
+    (num, lhs, vowel1, asterisk, vowel2, rhs) = match.groups()
+
+    out = '{^' + starters[lhs] + vowels[vowel1+vowel2] + enders[num+asterisk+rhs] + '^}'
+    if num=='#' and (lhs!='' or vowels!=''): out = out.replace('0','').upper()
+
+    return out
